f6bc3e54dcfa6f6e2d045f7e92971b3bdda22418
TommyNguyen <timsnguyen@hotmail.com>
2023-10-22 12:33:08 -0700

    add if/for statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { WishItem } from 'src/shared/models/wishItem';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = [
-    new WishItem("To Learn Angular"),
-    new WishItem("Get Coffee, true"),
-    new WishItem("Find grass that cuts itself")
+  items : WishItem[] = [
+    // new WishItem("To Learn Angular"),
+    // new WishItem("Get Coffee, true"),
+    // new WishItem("Find grass that cuts itself")
   ];
   title = 'wishlist';
 }
